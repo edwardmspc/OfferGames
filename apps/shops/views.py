0e40b88aa3853205dff670c093548858b6eb8642
edwardmspc <edward.mspc@gmail.com>
2015-03-21 11:41:53 -0430

    imprimiendo variables

--- apps/shops/views.py
+++ apps/shops/views.py
@@ -6,7 +6,7 @@ from django.views.generic import View
 class IndexLandingView(View):
 	def get(self, request,  *args, **kwargs):
 		articles = Article.objects.all()
-		
+
 		genres = Genre.objects.all()
 
-		return render(request, 'index.html')
+		return render(request, 'index.html', {'articles':articles,})
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ssh root@104.236.14.176
+cd ~/var/www/offergamesenv/offergames/
+git pull origin master
